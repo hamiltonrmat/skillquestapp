9a5b7e558abea701d3b71a6a6ae3fdcb52ccb389
Hamilton Araujo <117686961+hamiltonrmat@users.noreply.github.com>
2024-12-11 18:01:10 +0100

    Update Détail par compétence.py

--- pages/Détail par compétence.py
+++ pages/Détail par compétence.py
@@ -2,52 +2,81 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-liste_gen = pd.read_excel('https://github.com/hamiltonrmat/skillquestapp/raw/refs/heads/main/liste_gen.xlsx')[['prénom', 'NOM', 'clé', 'mail']]
-liste_ml = pd.read_excel('https://github.com/hamiltonrmat/skillquestapp/raw/refs/heads/main/liste_ml.xlsx')[['prénom', 'NOM', 'clé', 'mail']]
-edl = pd.read_excel('https://github.com/hamiltonrmat/skillquestapp/raw/refs/heads/main/edl_maths_skq.xlsx')
-file = pd.ExcelFile("edl_maths_skq.xlsx") 
-tables_edl = file.sheet_names
+liste_gen = pd.read_excel('liste_gen.xlsx')[['prénom', 'NOM', 'clé', 'mail']]
+liste_ml = pd.read_excel('liste_ml.xlsx')[['prénom', 'NOM', 'clé', 'mail']]
 
-############
-######## Maths du Lycée
-###########
 
-exam1_ml = pd.read_excel('https://github.com/hamiltonrmat/skillquestapp/raw/refs/heads/main/edl_maths_skq.xlsx', sheet_name=tables_edl[0])
+######### LIENS ######################
+
+# ML
+lien_resultats_ml = "resultats_ml.xlsx"
+resultats_ml = pd.read_excel(lien_resultats_ml)
+file = pd.ExcelFile(lien_resultats_ml) 
+tables_resultats_ml = file.sheet_names
+
+# FR
+lien_resultats_fr = "resultats_fr.xlsx"
+resultats_fr = pd.read_excel(lien_resultats_fr)
+file = pd.ExcelFile(lien_resultats_fr) 
+tables_resultats_fr = file.sheet_names
+
+# Dérivées
+lien_resultats_der = "resultats_der.xlsx"
+resultats_der = pd.read_excel(lien_resultats_der)
+file = pd.ExcelFile(lien_resultats_der) 
+tables_resultats_der = file.sheet_names
+
+# Primitives
+lien_resultats_prim = "resultats_prim.xlsx"
+resultats_prim = pd.read_excel(lien_resultats_prim)
+file = pd.ExcelFile(lien_resultats_prim) 
+tables_resultats_prim = file.sheet_names
+
+
+############ EXAMENS ##########################
+
+####### ML ##########
+exam1_ml = pd.read_excel(lien_resultats_ml, sheet_name=tables_resultats_ml[0])
 exam1_ml = exam1_ml[['Clé', 'Note/20,00']]
 exam1_ml.columns = ['clé', 'note']
 exam1_ml = exam1_ml.groupby(['clé']).max().reset_index()
 
-exam2_ml = pd.read_excel('https://github.com/hamiltonrmat/skillquestapp/raw/refs/heads/main/edl_maths_skq.xlsx', sheet_name=tables_edl[1])
+exam2_ml = pd.read_excel(lien_resultats_ml, sheet_name=tables_resultats_ml[1])
 exam2_ml = exam2_ml[['Clé', 'Note/20,00']]
 exam2_ml.columns = ['clé', 'note']
 exam2_ml = exam2_ml.groupby(['clé']).max().reset_index()
 
-exam3_ml = pd.read_excel('https://github.com/hamiltonrmat/skillquestapp/raw/refs/heads/main/edl_maths_skq.xlsx', sheet_name=tables_edl[2])
+exam3_ml = pd.read_excel(lien_resultats_ml, sheet_name=tables_resultats_ml[2])
 exam3_ml = exam3_ml[['Clé', 'Note/20,00']]
 exam3_ml.columns = ['clé', 'note']
 exam3_ml = exam3_ml.groupby(['clé']).max().reset_index()
 
-exam4_ml = pd.read_excel('https://github.com/hamiltonrmat/skillquestapp/raw/refs/heads/main/edl_maths_skq.xlsx', sheet_name=tables_edl[3])
+exam4_ml = pd.read_excel(lien_resultats_ml, sheet_name=tables_resultats_ml[3])
 exam4_ml = exam4_ml[['Clé', 'Note/20,00']]
 exam4_ml.columns = ['clé', 'note']
 exam4_ml = exam4_ml.groupby(['clé']).max().reset_index()
 
-exam5_ml = pd.read_excel('https://github.com/hamiltonrmat/skillquestapp/raw/refs/heads/main/edl_maths_skq.xlsx', sheet_name=tables_edl[4])
+exam5_ml = pd.read_excel(lien_resultats_ml, sheet_name=tables_resultats_ml[4])
 exam5_ml = exam5_ml[['Clé', 'Note/20,00']]
 exam5_ml.columns = ['clé', 'note']
 exam5_ml = exam5_ml.groupby(['clé']).max().reset_index()
 
-exam6_ml = pd.read_excel('https://github.com/hamiltonrmat/skillquestapp/raw/refs/heads/main/edl_maths_skq.xlsx', sheet_name=tables_edl[5])
+exam6_ml = pd.read_excel(lien_resultats_ml, sheet_name=tables_resultats_ml[5])
 exam6_ml = exam6_ml[['Clé', 'Note/20,00']]
 exam6_ml.columns = ['clé', 'note']
 exam6_ml = exam6_ml.groupby(['clé']).max().reset_index()
 
-exam7_ml = pd.read_excel('https://github.com/hamiltonrmat/skillquestapp/raw/refs/heads/main/edl_maths_skq.xlsx', sheet_name=tables_edl[6])
+exam7_ml = pd.read_excel(lien_resultats_ml, sheet_name=tables_resultats_ml[6])
 exam7_ml = exam7_ml[['Clé', 'Note/20,00']]
 exam7_ml.columns = ['clé', 'note']
 exam7_ml = exam7_ml.groupby(['clé']).max().reset_index()
 
-liste_exams_ML = [exam1_ml, exam2_ml, exam3_ml, exam4_ml, exam5_ml, exam6_ml, exam7_ml]
+exam8_ml = pd.read_excel(lien_resultats_ml, sheet_name=tables_resultats_ml[7])
+exam8_ml = exam8_ml[['Clé', 'Note/20,00']]
+exam8_ml.columns = ['clé', 'note']
+exam8_ml = exam8_ml.groupby(['clé']).max().reset_index()
+
+liste_exams_ML = [exam1_ml, exam2_ml, exam3_ml, exam4_ml, exam5_ml, exam6_ml, exam7_ml, exam8_ml]
 
 for x in range(len(liste_exams_ML)):
     liste_exams_ML[x].columns = ['clé', 'note_ml_t'+str(x+1)]
@@ -59,6 +88,7 @@ ml = ml.merge(exam4_ml, how='left', on='clé')
 ml = ml.merge(exam5_ml, how='left', on='clé')
 ml = ml.merge(exam6_ml, how='left', on='clé')
 ml = ml.merge(exam7_ml, how='left', on='clé')
+ml = ml.merge(exam8_ml, how='left', on='clé')
 
 ml['note_max'] = ml.max(axis='columns', numeric_only=True)
 
@@ -74,33 +104,48 @@ for i in range(ml.shape[0]):
 
 ml['validation'] = validations_ml
 
+xp = []
+for i in range(ml.shape[0]):
+    if validations_ml[i] == 'pas_de_tentative':
+        xp.append('pas_de_tentative')
+    elif validations_ml[i] == 'pas_validé':
+        xp.append('pas_validé')
+    elif [ml['note_max'][i] for i in range(ml.shape[0])][i] == 20:
+         xp.append('Argent')
+    else:
+        xp.append('Bronze')
+ml['xp'] = xp
 
-##################
-#### Fonctions réelles
-##################
+
+#### Fonctions réelles ##########
 
 
-exam1_fr = pd.read_excel('https://github.com/hamiltonrmat/skillquestapp/raw/refs/heads/main/edl_maths_skq.xlsx', sheet_name=tables_edl[7])
+exam1_fr = pd.read_excel(lien_resultats_fr, sheet_name=tables_resultats_fr[0])
 exam1_fr = exam1_fr[['Clé', 'Note/20,00']]
 exam1_fr.columns = ['clé', 'note']
 exam1_fr = exam1_fr.groupby(['clé']).max().reset_index()
 
-exam2_fr = pd.read_excel('https://github.com/hamiltonrmat/skillquestapp/raw/refs/heads/main/edl_maths_skq.xlsx', sheet_name=tables_edl[8])
+exam2_fr = pd.read_excel(lien_resultats_fr, sheet_name=tables_resultats_fr[1])
 exam2_fr = exam2_fr[['Clé', 'Note/20,00']]
 exam2_fr.columns = ['clé', 'note']
 exam2_fr = exam2_fr.groupby(['clé']).max().reset_index()
 
-exam3_fr = pd.read_excel('https://github.com/hamiltonrmat/skillquestapp/raw/refs/heads/main/edl_maths_skq.xlsx', sheet_name=tables_edl[9])
+exam3_fr = pd.read_excel(lien_resultats_fr, sheet_name=tables_resultats_fr[2])
 exam3_fr = exam3_fr[['Clé', 'Note/20,00']]
 exam3_fr.columns = ['clé', 'note']
 exam3_fr = exam3_fr.groupby(['clé']).max().reset_index()
 
-exam4_fr = pd.read_excel('https://github.com/hamiltonrmat/skillquestapp/raw/refs/heads/main/edl_maths_skq.xlsx', sheet_name=tables_edl[10])
+exam4_fr = pd.read_excel(lien_resultats_fr, sheet_name=tables_resultats_fr[3])
 exam4_fr = exam4_fr[['Clé', 'Note/20,00']]
 exam4_fr.columns = ['clé', 'note']
 exam4_fr = exam4_fr.groupby(['clé']).max().reset_index()
 
-liste_exams_FR = [exam1_fr, exam2_fr, exam3_fr, exam4_fr]
+exam5_fr = pd.read_excel(lien_resultats_fr, sheet_name=tables_resultats_fr[4])
+exam5_fr = exam5_fr[['Clé', 'Note/20,00']]
+exam5_fr.columns = ['clé', 'note']
+exam5_fr = exam5_fr.groupby(['clé']).max().reset_index()
+
+liste_exams_FR = [exam1_fr, exam2_fr, exam3_fr, exam4_fr, exam5_fr]
 
 for x in range(len(liste_exams_FR)):
     liste_exams_FR[x].columns = ['clé', 'note_fr_t'+str(x+1)]
@@ -109,6 +154,7 @@ fr = liste_gen.merge(exam1_fr, how='left', on='clé')
 fr = fr.merge(exam2_fr, how='left', on='clé')
 fr = fr.merge(exam3_fr, how='left', on='clé')
 fr = fr.merge(exam4_fr, how='left', on='clé')
+fr = fr.merge(exam5_fr, how='left', on='clé')
 
 fr['note_max'] = fr.max(axis='columns', numeric_only=True)
 
@@ -122,28 +168,45 @@ for i in range(fr.shape[0]):
         validations_fr.append('pas_validé')
 fr['validation'] = validations_fr
 
+xp = []
+for i in range(fr.shape[0]):
+    if validations_fr[i] == 'pas_de_tentative':
+        xp.append('pas_de_tentative')
+    elif validations_fr[i] == 'pas_validé':
+        xp.append('pas_validé')
+    elif [fr['note_max'][i] for i in range(fr.shape[0])][i] == 20:
+         xp.append('Argent')
+    else:
+        xp.append('Bronze')
+fr['xp'] = xp
 
-##############
-### Dérivées
-#############
 
-exam1_der = pd.read_excel('https://github.com/hamiltonrmat/skillquestapp/raw/refs/heads/main/edl_maths_skq.xlsx', sheet_name=tables_edl[11])
+########### Dérivées
+
+
+exam1_der = pd.read_excel(lien_resultats_der, sheet_name=tables_resultats_der[0])
 exam1_der = exam1_der[['Clé', 'Note/20,00']]
 exam1_der.columns = ['clé', 'note']
 exam1_der = exam1_der.groupby(['clé']).max().reset_index()
 
-exam2_der = pd.read_excel('https://github.com/hamiltonrmat/skillquestapp/raw/refs/heads/main/edl_maths_skq.xlsx', sheet_name=tables_edl[12])
+exam2_der = pd.read_excel(lien_resultats_der, sheet_name=tables_resultats_der[1])
 exam2_der = exam2_der[['Clé', 'Note/20,00']]
 exam2_der.columns = ['clé', 'note']
 exam2_der = exam2_der.groupby(['clé']).max().reset_index()
 
-liste_exams_der = [exam1_der, exam2_der]
+exam3_der = pd.read_excel(lien_resultats_der, sheet_name=tables_resultats_der[2])
+exam3_der = exam3_der[['Clé', 'Note/20,00']]
+exam3_der.columns = ['clé', 'note']
+exam3_der = exam3_der.groupby(['clé']).max().reset_index()
+
+liste_exams_der = [exam1_der, exam2_der, exam3_der]
 
 for x in range(len(liste_exams_der)):
     liste_exams_der[x].columns = ['clé', 'note_der_t'+str(x+1)]
 
 der = liste_gen.merge(exam1_der, how='left', on='clé')
 der = der.merge(exam2_der, how='left', on='clé')
+der = der.merge(exam3_der, how='left', on='clé')
 
 der['note_max'] = der.max(axis='columns', numeric_only=True)
 
@@ -157,13 +220,70 @@ for i in range(der.shape[0]):
         validations_der.append('pas_validé')
 der['validation'] = validations_der
 
+xp = []
+for i in range(der.shape[0]):
+    if validations_der[i] == 'pas_de_tentative':
+        xp.append('pas_de_tentative')
+    elif validations_der[i] == 'pas_validé':
+        xp.append('pas_validé')
+    elif [der['note_max'][i] for i in range(der.shape[0])][i] == 20:
+         xp.append('Argent')
+    else:
+        xp.append('Bronze')
+der['xp'] = xp
 
-competences = ["Maths du Lycée", "Fonctions Réelles", "Dérivées"]
-df_competences = [ml, fr, der]
 
+############ Primitives
+
+exam1_prim = pd.read_excel(lien_resultats_prim, sheet_name=tables_resultats_prim[0])
+exam1_prim = exam1_prim[['Clé', 'Note/20,00']]
+exam1_prim.columns = ['clé', 'note']
+exam1_prim = exam1_prim.groupby(['clé']).max().reset_index()
+
+liste_exams_prim = [exam1_prim]
+
+for x in range(len(liste_exams_prim)):
+    liste_exams_prim[x].columns = ['clé', 'note_prim_t'+str(x+1)]
+
+prim = liste_gen.merge(exam1_prim, how='left', on='clé')
+#prim = prim.merge(exam2_prim, how='left', on='clé')
+
+prim['note_max'] = prim.max(axis='columns', numeric_only=True)
+
+validations_prim = []
+for i in range(prim.shape[0]):
+    if prim['note_max'].isnull()[i] == True:
+        validations_prim.append('pas_de_tentative')
+    elif prim['note_max'][i] >= 11:
+        validations_prim.append('validé')
+    else:
+        validations_prim.append('pas_validé')
+prim['validation'] = validations_prim
+
+xp = []
+for i in range(prim.shape[0]):
+    if validations_prim[i] == 'pas_de_tentative':
+        xp.append('pas_de_tentative')
+    elif validations_prim[i] == 'pas_validé':
+        xp.append('pas_validé')
+    elif [prim['note_max'][i] for i in range(prim.shape[0])][i] == 20:
+         xp.append('Argent')
+    else:
+        xp.append('Bronze')
+prim['xp'] = xp
+
+
+
+#### APP #####
+
+
+competences = ["Maths du Lycée", "Fonctions Réelles", "Dérivées", "Primitives"]
+df_competences = [ml, fr, der, prim]
 
 
 options = st.multiselect("Selectioner une compétence:",competences)
+
+st.write("Dernière mise à jour: 11/12/2024") 
 
 for comp in options:
     df_comp = df_competences[competences.index(comp)]
@@ -189,15 +309,14 @@ for comp in options:
                 "Pas de tentatives",
             ], key=comp)
         if genre == "Compétence validé":
-            val = df_comp[df_comp['validation'] == 'validé'][['clé', 'note_max', 'validation']]
+            val = df_comp[df_comp['validation'] == 'validé'][['clé', 'mail', 'note_max', 'validation', 'xp']]
             st.dataframe(val)
             st.write(str(val.shape[0])+' étudiants')
         if genre == "Compétence pas validé":
-            pas_val = df_comp[df_comp['validation'] == 'pas_validé'][['clé', 'note_max', 'validation']]
+            pas_val = df_comp[df_comp['validation'] == 'pas_validé'][['clé', 'mail', 'note_max', 'validation', 'xp']]
             st.dataframe(pas_val)
             st.write(str(pas_val.shape[0])+' étudiants')
         if genre == "Pas de tentatives":
-            pas_ten = df_comp[df_comp['validation'] == 'pas_de_tentative'][['clé', 'validation']]
+            pas_ten = df_comp[df_comp['validation'] == 'pas_de_tentative'][['clé', 'mail', 'validation', 'xp']]
             st.dataframe(pas_ten)
             st.write(str(pas_ten.shape[0])+' étudiants')
-            
